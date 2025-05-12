6cd897c9e3320520da03ba59aa7d7038acff3549
Sonu Jha <159139463+Sonujha2808@users.noreply.github.com>
2025-05-12 19:24:34 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -75,13 +75,13 @@ mongoose
   .catch((err) => console.error("MongoDB connection error:", err));
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client", "build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
